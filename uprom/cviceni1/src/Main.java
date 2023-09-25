fbd8c6fef3bb59d8fe59eee79ad262a7c25421fd
OndrejHana <ondrahana001@gmail.com>
2023-09-25 11:13:19 +0200

    TNPW cv2

---
+++ uprom/cviceni1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        var a = "Hello world";
+        a = "pepa";
+
+        System.out.println(add(10,12));
+
+    }
+    static int add(int a, int b) {
+        return a + b;
+    }
+}
